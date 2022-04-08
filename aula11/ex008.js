aa5a8355340d78655c93f06ce5b8fc57db1b7b1a
adalton <adaltonvitor9@gmail.com>
2022-04-07 21:26:23 -0300

    apremdemdo a manipular o dom e condiçoe javascriot

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70.5
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 70){
+    console.log('Voce ultrapassou a velocidade permitida!MULTADO')
+}
+console.log('Dirija sempre usando o cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+if(pais == 'Brasil'){
+    console.log('Voce e brasilero')
+}else{
+    console.log('Voce e estrangeiro')
+}
