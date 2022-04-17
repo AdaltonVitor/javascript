c68b359c966c3fc915960bbcfe957d841f2dc1b5
adalton <adaltonvitor9@gmail.com>
2022-04-17 20:00:17 -0300

    projeto 1 js

---
+++ aula12-ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar(){
+    var msg =window.document.getElementById('msg')
+    var img =window.document.getElementById('imagen')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 6
+   
+    msg.innerHTML = `agora sao ${hora} horas`
+    if(hora >= 0 && hora < 12 ){
+        //bomdia
+        img.src = 'foto-manha.png'
+        document.body.style.background = 'rgb(235, 223, 200)'
+    }else if(hora >= 12 && hora < 18){
+        //boa tarde
+        img.src = 'foto-tarde.png'
+    }else{
+        //boa noite
+        img.src = 'foto-noite.png'
+        document.body.style.background = '#332924'
+    }
+}
